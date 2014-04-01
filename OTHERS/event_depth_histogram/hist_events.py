8216d6fd95ac43768ba920a8ca689bb416130da5
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2014-04-01 13:20:54 +0200

    try to adjust the fonts...

--- OTHERS/event_depth_histogram/hist_events.py
+++ OTHERS/event_depth_histogram/hist_events.py
@@ -41,7 +41,7 @@ counter = {}
 #for i in counter.keys():
 #        plt.bar(int(i)-0.5, counter[i], 1)
 #
-
+counting = 0
 for i in range(len(events)):
     if events[i]['datetime'] > max_date: 
         print '%s > %s' %(events[i]['datetime'], max_date)
@@ -50,17 +50,21 @@ for i in range(len(events)):
         counter[str(-int(events[i]['depth']))] = 1
     else:
         counter[str(-int(events[i]['depth']))] += 1
+    counting += 1
+
+print 'Number of all used events: %s' % counting
 for i in counter.keys():
     #if 100 <= counter[i]:
         #plt.bar(int(i)-0.5, counter[i], 1, log=True)
         plt.bar(int(i)-0.5, counter[i], 1)
 
+plt.ion()
 plt.xlim(xmin=-2, xmax=100)
 plt.ylim(ymax=1100)
-plt.xlabel('Depth(km)', size='x-large', weight='bold')
-plt.ylabel('Number of events', size='x-large', weight='bold')
-plt.xticks(size='x-large', weight='bold')
-plt.yticks(size='x-large', weight='bold')
-plt.title('Depth=10km (1002 events)\nDepth=33km (254 events)\n', size='x-large', weight='bold')
+plt.xlabel('Depth (km)', size=36, weight='bold')
+plt.ylabel('Number of events', size=36, weight='bold')
+plt.xticks(size=32, weight='bold')
+plt.yticks(size=32, weight='bold')
+plt.title('Depth=10km (1002 events)    Depth=33km (254 events)\n', size=36, weight='bold')
 plt.show()
 
